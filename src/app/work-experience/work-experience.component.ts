ce7e9f71a85854d5536f48dd4e187652ec3b0775
MiguelLaraB <zS22004395@estudiantes.uv.mx>
2024-09-25 18:05:59 -0600

    hola

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@Component({
-  selector: 'app-work-experience',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './work-experience.component.html',
-  styleUrl: './work-experience.component.css'
-})
-export class WorkExperienceComponent {
-  workExperience : Array<any> = [];
-  ngOnInit(): void {
-    let work1 = {
-      fecha: "2024-2024",
-      ubicacion: "Ixtac Ver. ",
-      puesto: "Estudiante",
-      empresa: "UV",
-      logros: [
-        { descripcion : "Creación de página web de logros"},
-        { descripcion: "Conexión y consumo de la API de Steam"}
-      ]
-    };
-
-    let work2 = {
-      fecha: "2024-2024",
-      ubicacion: "Orizaba, Ver. ",
-      puesto: "Estudiante",
-      empresa: "UV",
-      logros: [
-        { descripcion : "Creación de página web de economía"},
-        { descripcion: "Creación de diagramas de casos de uso"}
-      ]
-    };
-
-    this.workExperience.push(work1);
-    this.workExperience.push(work2);
-    console.log(this.workExperience)
-  }
-
-}
-=======
 
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -84,4 +41,3 @@ export class WorkExperienceComponent {
 
 }
 
->>>>>>> 6986dc767429db80bc6744b5ec17f32ee733655d
